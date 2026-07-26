0740d7934c50db8091ef08dccf702ca41ac2867a
DevXstyler <205496511+DevXstyler@users.noreply.github.com>
2026-07-27 00:27:19 +0200

    Move CommandHandler ownership into App

--- include/App.hpp
+++ include/App.hpp
@@ -1,9 +1,14 @@
 #pragma once 
 
+#include "CommandHandler.hpp"
+
 class App {
     public:
         void run();
     private:
         void showBanner();
         void handleInput();
+
+        // Create a object of the CommandHandler Class
+        CommandHandler handler;
 };
--- src/App.cpp
+++ src/App.cpp
@@ -1,5 +1,5 @@
 #include "App.hpp"
-#include "CommandHandler.hpp"
+// #include "CommandHandler.hpp" < Not needed anymore, included in App.hpp (line 3)
 
 #include <iostream>
 
@@ -14,18 +14,16 @@ void App::showBanner(){
                                                 ______  _______ _    _  _____  _     _ _______ _______ _______
                                                 |     \ |______  \  /  |   __| |     | |______ |______    |   
                                                 |_____/ |______   \/   |____\| |_____| |______ ______|    |   
-                                            V. 0.0.1 - Created by DevXstyler - https://github.com/devxstyler/devquest
+                                            V. 0.0.2 - Created by DevXstyler - https://github.com/devxstyler/devquest
     )";
 }
 
 void App::handleInput(){
-    std::string user_input = ""; 
+    std::string user_input; 
     std::cout << "What are your Orders, master?: ";
     std::getline(std::cin, user_input); // Use this, instead of: `std::cin >> user_input` | cin only reads the 1st word, it ends after a space
     
-    user_input = user_input.tolower();
-    // Create a .. of the CommandHandler Class and pass the user_input to it.
-    CommandHandler handler;
+    // use the object created in App.hpp and pass the user_input to it.
     handler.handleCommand(user_input);
 
 }
